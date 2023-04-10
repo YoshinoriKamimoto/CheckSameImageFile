2b8436df94fd2a8d3824af2e05c8508c887f8528
YoshinoriKamimoto <k.yoshinori0210@gmail.com>
2023-04-10 10:24:58 +0900

    画像ファイル同一チェックサンプルプログラム

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawImg1Button = new System.Windows.Forms.Button();
+            this.DrawImg2Button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.DeleteImgButton = new System.Windows.Forms.Button();
+            this.CompareImgButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DrawImg1Button
+            // 
+            this.DrawImg1Button.Location = new System.Drawing.Point(145, 49);
+            this.DrawImg1Button.Name = "DrawImg1Button";
+            this.DrawImg1Button.Size = new System.Drawing.Size(75, 23);
+            this.DrawImg1Button.TabIndex = 0;
+            this.DrawImg1Button.Text = "画像1";
+            this.DrawImg1Button.UseVisualStyleBackColor = true;
+            this.DrawImg1Button.Click += new System.EventHandler(this.DrawImg1Button_Click);
+            // 
+            // DrawImg2Button
+            // 
+            this.DrawImg2Button.Location = new System.Drawing.Point(452, 49);
+            this.DrawImg2Button.Name = "DrawImg2Button";
+            this.DrawImg2Button.Size = new System.Drawing.Size(75, 23);
+            this.DrawImg2Button.TabIndex = 1;
+            this.DrawImg2Button.Text = "画像2";
+            this.DrawImg2Button.UseVisualStyleBackColor = true;
+            this.DrawImg2Button.Click += new System.EventHandler(this.DrawImg2Button_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(47, 99);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(271, 217);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(350, 99);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(271, 217);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.WaitOnLoad = true;
+            // 
+            // DeleteImgButton
+            // 
+            this.DeleteImgButton.Location = new System.Drawing.Point(293, 381);
+            this.DeleteImgButton.Name = "DeleteImgButton";
+            this.DeleteImgButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteImgButton.TabIndex = 4;
+            this.DeleteImgButton.Text = "画像削除";
+            this.DeleteImgButton.UseVisualStyleBackColor = true;
+            this.DeleteImgButton.Click += new System.EventHandler(this.DeleteImgButton_Click);
+            // 
+            // CompareImgButton
+            // 
+            this.CompareImgButton.Location = new System.Drawing.Point(293, 352);
+            this.CompareImgButton.Name = "CompareImgButton";
+            this.CompareImgButton.Size = new System.Drawing.Size(75, 23);
+            this.CompareImgButton.TabIndex = 5;
+            this.CompareImgButton.Text = "画像比較";
+            this.CompareImgButton.UseVisualStyleBackColor = true;
+            this.CompareImgButton.Click += new System.EventHandler(this.CompareImgButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 462);
+            this.Controls.Add(this.CompareImgButton);
+            this.Controls.Add(this.DeleteImgButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.DrawImg2Button);
+            this.Controls.Add(this.DrawImg1Button);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DrawImg1Button;
+        private System.Windows.Forms.Button DrawImg2Button;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button DeleteImgButton;
+        private System.Windows.Forms.Button CompareImgButton;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Image image1;
+        private Image image2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        // 画像1表示
+        private void DrawImg1Button_Click(object sender, EventArgs e)
+        {
+            // 画像1表示
+            using (FileStream fs = File.OpenRead(@"C:\Users\kamimoto\Pictures\img1\sample5.png"))
+            {
+                image1 = Image.FromStream(fs);
+                pictureBox1.Image = image1;
+            }
+        }
+
+
+        // 画像2表示
+        private void DrawImg2Button_Click(object sender, EventArgs e)
+        {
+            // 画像2表示
+            using (FileStream fs = File.OpenRead(@"C:\Users\kamimoto\Pictures\img1\sample6.png"))
+            {
+                image2 = Image.FromStream(fs);
+                pictureBox2.Image = image2;
+            }
+        }
+
+
+        // 画像比較
+        private void CompareImgButton_Click(object sender, EventArgs e)
+        {
+            // 画像1のバイトを取得
+            byte[] bytes1;
+            using (FileStream fs = File.OpenRead(@"C:\Users\kamimoto\Pictures\img1\sample5.png"))
+            {
+                bytes1 = new byte[fs.Length];
+                fs.Read(bytes1, 0, bytes1.Length);
+                int sum = 0;
+                foreach (byte b in bytes1)
+                {
+                    sum += b;
+                }
+                Console.WriteLine($"バイト合計(画像1)：{sum}");
+            }
+
+            // 画像2のバイトを取得
+            byte[] bytes2;
+            using (FileStream fs = File.OpenRead(@"C:\Users\kamimoto\Pictures\img1\sample6.png"))
+            {
+                bytes2 = new byte[fs.Length];
+                fs.Read(bytes2, 0, bytes2.Length);
+                int sum = 0;
+                foreach (byte b in bytes2)
+                {
+                    sum += b;
+                }
+                Console.WriteLine($"バイト合計(画像2)：{sum}");
+            }
+
+
+            Console.WriteLine($"要素数(画像1):{bytes1.Length}");
+            Console.WriteLine($"要素数(画像2):{bytes2.Length}");
+
+            // 画像1,画像2同一ファイル比較
+            if (bytes1.Length != bytes2.Length)
+            {
+                Console.WriteLine("画像不一致");
+                MessageBox.Show("画像不一致");
+                return;
+            }
+
+            for (int i = 0; i < bytes1.Length; i++)
+            {
+                if (bytes1[i] != bytes2[i])
+                {
+                    Console.WriteLine("画像不一致");
+                    MessageBox.Show("画像不一致");
+                    return;
+                }
+            }
+
+            Console.WriteLine("画像一致");
+            MessageBox.Show("画像一致");
+
+        }
+
+
+        // 画像削除
+        private void DeleteImgButton_Click(object sender, EventArgs e)
+        {
+            ReleaseImageResource();
+        }
+
+
+        // フォームクローズ
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ReleaseImageResource();
+        }
+
+
+        // 画像リソース解放
+        private void ReleaseImageResource()
+        {
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.Dispose();
+                pictureBox1.Image = null;
+            }
+
+            if (pictureBox2.Image != null)
+            {
+                pictureBox2.Image.Dispose();
+                pictureBox2.Image = null;
+            }
+
+            if (this.image1 != null)
+            {
+                this.image1.Dispose();
+                this.image1 = null;
+            }
+
+            if (this.image2 != null)
+            {
+                this.image2.Dispose();
+                this.image2 = null;
+            }
+        }
+    }
+}
